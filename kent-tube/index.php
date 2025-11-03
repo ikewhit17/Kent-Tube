8318d707e643ad05d7e0172568dd8d01a80d372b
Isaac Whitmarsh <iwhitmar@kent.edu>
2025-11-03 14:32:37 -0500

    Updated name on index, moved files to correct folder

--- kent-tube/index.php
+++ kent-tube/index.php
@@ -1,13 +1,12 @@
 <?php
   include("database.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>registration</title>
 </head>
 <body>
     <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
